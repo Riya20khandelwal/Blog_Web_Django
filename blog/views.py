27968b9d941f38f585ae6ae91430984e6a84be58
Riya20khandelwal <riyakhandelwal204@gmail.com>
2024-07-22 17:08:49 +0530

    add comments to blogs

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import PostModel
-from .forms import PostModelForm, PostUpdateForm
+from .forms import PostModelForm, PostUpdateForm, CommentForm
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -29,8 +29,19 @@ def index(request):
 
 def post_detail(request, pk):
     post = PostModel.objects.get(id=pk)
+    if request.method == "POST":
+        c_form = CommentForm(request.POST)
+        if c_form.is_valid():
+            instance = c_form.save(commit=False)
+            instance.user = request.user
+            instance.post = post
+            instance.save()
+            return redirect('blog-post-detail', pk=post.id)
+    else:
+        c_form = CommentForm()
     context = {
         'post':post,
+        'c_form':c_form,
     }
     return render(request, 'blog/post_detail.html', context)
 
